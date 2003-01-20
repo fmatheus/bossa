aee62b62c9cee6133d39cfc013cc47f280679f76
gdvieira <gdvieira@localhost>
2003-01-20 18:01:52 -0200

    Added the some methods to CaseTypeManager and coded the tests. The implementation is still needed.

--- src/main/com/bigbross/bossa/wfnet/CaseTypeManager.java
+++ src/main/com/bigbross/bossa/wfnet/CaseTypeManager.java
@@ -24,6 +24,9 @@
 
 package com.bigbross.bossa.wfnet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This class manages all registered case types of the workflow
  * system. It provides all the case type life cycle functions. <p>
@@ -31,5 +34,44 @@ package com.bigbross.bossa.wfnet;
  * @author <a href="http://www.bigbross.com">BigBross Team</a>
  */
 public class CaseTypeManager {
+    
+    Map caseTypes;
+    
+    public CaseTypeManager() {
+        caseTypes = new HashMap();
+    }
 
+    /**
+     * Registers a new case type in the manager. <p>
+     * 
+     * @param The CaseType object containig the case type.
+     * @return <code>true</code> if the case type is registered,
+     *         <code>false</code> if there is already a case type
+     *         registered with the same id.
+     */    
+    public boolean registerCaseType(CaseType caseType) {
+        return false;
+    }
+    
+    /**
+     * Removes the case type from the manager. This operation will remove also
+     * <emph>all</emph> cases of this case type.
+     * 
+     * @param id The id of the case type.
+     */
+    public void removeCaseType(String id) {
+    }
+    
+    /**
+     * Returns the case type with the provided id. <p>
+     * 
+     * FIXME: Maybe this is the place to provide a facade do the prevalent
+     * system, instead of giving access to the case type itself. <p>
+     * 
+     * @param id The id of the desired case type.
+     * @return The case type if it exists, <code>null</code> otherwise. 
+     */
+    public CaseType getCaseType(String id) {
+        return null;
+    }
 }
---
+++ src/testcases/com/bigbross/bossa/wfnet/CaseTypeManagerTest.java
@@ -0,0 +1,64 @@
+/*
+ * Bossa Workflow System
+ *
+ * $Id$
+ *
+ * Copyright (C) 2003 OpenBR Sistemas S/C Ltda.
+ *
+ * This file is part of Bossa.
+ *
+ * Bossa is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+package com.bigbross.bossa.wfnet;
+
+import junit.framework.TestCase;
+
+public class CaseTypeManagerTest extends TestCase {
+
+    public CaseTypeManagerTest(String name) {
+	super(name);
+    }
+
+    protected void setUp() {
+    	System.out.println("Setting up a case type manager test.");
+    }
+
+    public void testRegisterCaseType() {
+        CaseTypeManager caseTypeManager = new CaseTypeManager();
+        
+        assertTrue(caseTypeManager.registerCaseType(
+                        new CaseTypeTest("hohoho").createTestCaseType("test1")));
+        assertFalse(caseTypeManager.registerCaseType(
+                        new CaseTypeTest("hohoho").createTestCaseType("test1")));
+    }
+    
+    public void testQueryCaseType() {
+        CaseTypeManager caseTypeManager = new CaseTypeManager();
+        CaseType expected = new CaseTypeTest("hohoho").createTestCaseType("test1");
+        
+        assertTrue(caseTypeManager.registerCaseType(expected));
+        assertSame(expected, caseTypeManager.getCaseType("test1"));
+    }
+    
+    public void testRemoveCaseType() {
+        CaseTypeManager caseTypeManager = new CaseTypeManager();
+        CaseType expected = new CaseTypeTest("hohoho").createTestCaseType("test1");
+        
+        assertTrue(caseTypeManager.registerCaseType(expected));
+        caseTypeManager.removeCaseType("test1");
+        assertNull(caseTypeManager.getCaseType("test1"));
+    }
+}
